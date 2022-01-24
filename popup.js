c745c32e42a71fd4ceab80c2f2691cd8f0c5d71d
Denis Feliciano <denisf.salles@gmail.com>
2022-01-23 23:55:09 -0300

    feat: update extension with html version code. Refactor code.

--- popup.js
+++ popup.js
@@ -1,50 +1,68 @@
+
 // Initialize butotn with users's prefered color
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.set({ customURL: [] });
+let addToPageButton = document.getElementById('changeColor');
+let fakeData = ['www.g1.com'];
 
-chrome.storage.sync.get('color', ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+chrome.storage.sync.set({ customURL: fakeData });
+renderUrlList();
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener('click', async () => {
+// When the button is clicked, inject setCurrentTabURL into current page
+addToPageButton.addEventListener('click', async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: setCurrentTabURL,
   });
 
-  chrome.storage.sync.get('customURL', (data) => {
-    let urls = data.customURL;
-     addWebSites(urls[urls.length - 1]);
-  });
-  // changeColor.style.backgroundColor = 'orange';
-  // changeColor.innerText = 'Olá Mundo';
+  updateFakeDataFromAsyncStorage();
+  renderUrlList();
 });
+
+function updateFakeDataFromAsyncStorage() {
+  chrome.storage.sync.get('customURL', (data) => {
+    fakeData = data.customURL;
+  });
+}
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-function setPageBackgroundColor() {
+function setCurrentTabURL() {
   chrome.storage.sync.get('customURL', (data) => {
     let urls = data.customURL;
     let newSiteURL = window.location.href;
 
     if (newSiteURL == urls[urls.length - 1]) return;
 
-    urls = [...urls, newSiteURL];
+    fakeData = [...urls, newSiteURL];
+    console.log(fakeData);
     // update on local storage
-    chrome.storage.sync.set({ customURL: urls });
+    chrome.storage.sync.set({ customURL: fakeData });
   });
 }
 
-function addWebSites(siteName) {
+function renderUrlList() {
   let sitesListHtmlElement = document.querySelector('[data-sites="urls"');
-  let li = document.createElement('li');
-  li.textContent = siteName;
-  sitesListHtmlElement.appendChild(li);
+  sitesListHtmlElement.innerHTML = '';
+  fakeData.forEach((url) => {
+    let li = document.createElement('li');
+    li.classList.add('list-url');
+    let link = document.createElement('a');
+    link.href = url;
+    link.textContent = url;
+    link.target = '_blank';
+    li.appendChild(link);
+    sitesListHtmlElement.appendChild(li);
+  });
+  fetchData();
 }
 
 function checkUrlsAreEquals(url1, url2) {
   return url1 === url2;
+}
+
+function fetchData() {
+  fetch('http://localhost:8080/?q=1234')
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 }
